706588ced018e3c47b8c4e6065a1f7a58f0e5f6d
gyanendra9984 <mani15102002@gmail.com>
2024-03-13 16:21:35 +0530

    deploy code added

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 // Define a route to send the chats array
-app.get('/', (req, res) => {
-    res.send("API is running");
-});
+// app.get('/', (req, res) => {
+//     res.send("API is running");
+// });
 
 app.use('/api/user', userRoutes);
 app.use("/api/chat", chatRoutes);
@@ -25,6 +25,17 @@ app.use("/api/message", messageRoutes);
 // ---------------------------deployment--------------------------------
 
 const __dirname1 = path.resolve();
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.resolve(__dirname1, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "..","frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
+
 
 
 // ---------------------------deployment--------------------------------
